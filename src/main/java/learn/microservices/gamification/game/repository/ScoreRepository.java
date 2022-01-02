15a45007bc97a3c39b55dcd7197384737a8ca7e2
olegyev <o.yevstratyev@gmail.com>
2022-01-02 18:04:11 +0300

    Implement ScoreRepository#sumTotalScoreForUser

--- src/main/java/learn/microservices/gamification/game/repository/ScoreRepository.java
+++ src/main/java/learn/microservices/gamification/game/repository/ScoreRepository.java
@@ -2,17 +2,29 @@ package learn.microservices.gamification.game.repository;
 
 import learn.microservices.gamification.game.entity.LeaderBoardRow;
 import learn.microservices.gamification.game.entity.ScoreCard;
+import lombok.Data;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ScoreRepository extends MongoRepository<ScoreCard, String> {
 
-    Optional<Integer> getTotalScoreForUser(String userId);
+    /**
+     * @return the total score for a given user (the sum of the scores of all their ScoreCards),
+     * empty if user doesn't exist
+     */
+    @Aggregation(pipeline = {"{$match: {userId: '?0'}}", "{$group: {_id: 'userTotalScore', totalScore: {$sum: '$score'}}}"})
+    AggregationResults<UserTotalScore> sumTotalScoreForUser(String userId);
 
     List<ScoreCard> findByUserIdOrderByTimestampDesc(String userId);
 
     List<LeaderBoardRow> findFirst10();
+
+    @Data
+    class UserTotalScore {
+        private Integer totalScore;
+    }
 
 }
