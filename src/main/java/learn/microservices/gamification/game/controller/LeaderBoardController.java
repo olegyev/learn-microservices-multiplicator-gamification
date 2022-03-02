6b63d59fb7086d2d0642f16b8e6a291b12aa7348
olegyev <o.yevstratyev@gmail.com>
2022-03-02 18:29:49 +0300

    Edit comments

--- src/main/java/learn/microservices/gamification/game/controller/LeaderBoardController.java
+++ src/main/java/learn/microservices/gamification/game/controller/LeaderBoardController.java
@@ -3,6 +3,7 @@ package learn.microservices.gamification.game.controller;
 import learn.microservices.gamification.game.entity.LeaderBoardRow;
 import learn.microservices.gamification.game.service.LeaderBoardService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +13,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/leaders")
 @RequiredArgsConstructor
+@Slf4j
 public class LeaderBoardController {
 
     private final LeaderBoardService leaderBoardService;
 
     @GetMapping
     public List<LeaderBoardRow> getLeaderBoard() {
+        log.info("Get leaderboard request processing");
         return leaderBoardService.getCurrentLeaderBoard();
     }
 
